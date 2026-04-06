778084e72a02848c35e55c0af6c1f7008b9aa80f
omonievusacollins-max <omonievusacollins@gmail.com>
2026-04-06 01:29:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+
+  return (
+    <>
+     <Button text="click me"/>
+     {/* <button>Click me</button> */}
+    </>
+  )
+}
+
+export default App
