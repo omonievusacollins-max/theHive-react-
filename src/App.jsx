b9c1b7cf715ed09101b31dde3c5a957c9625b458
omonievusacollins-max <omonievusacollins@gmail.com>
2026-04-22 13:41:03 -0400

    Updated files and created App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
-import Button from './components/Button';
-
+// import Button from './components/Button';
+import Card from './components/Cards/Card';
+import Logo from './components/Cards/Logo'
+import NavItem from './components/Buttons/NavItem';
+import CategoryCard from './components/Cards/CategoryCard';
 function App() {
 
   return (
     <>
-     <Button text="click me"/>
-     {/* <button>Click me</button> */}
+    <Card description={'Total items'}  amount={125} svg={'/assets/BurgerIcon.svg'}/>
+    <Logo/>
+    <NavItem text={'Caterogy'} src={'/assets/Stack.svg'}/>
+    <CategoryCard src={'/assets/BugerImage.png'} color='red'/>
     </>
   )
 }
