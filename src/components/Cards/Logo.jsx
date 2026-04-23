68ce8f5386d47dd5cc998dd2166729ae5ec1e910
omonievusacollins-max <omonievusacollins@gmail.com>
2026-04-23 01:45:53 -0400

    Created logo component

---
+++ src/components/Cards/Logo.jsx
@@ -0,0 +1,10 @@
+import './logo.css'
+function Logo(){
+    return(
+        <div className='logoContainer'>
+            <img src="/assets/Burgerlogo.svg" alt="" className='Burgerlogo'/>
+            <img src="/assets/Hive-logo.svg" alt="logo" className='logo'/>
+        </div>
+    )
+}
+export default Logo
