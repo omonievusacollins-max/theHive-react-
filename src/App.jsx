39076d71e3b3517c70cfbcf9386590d04174f893
omonievusacollins-max <omonievusacollins@gmail.com>
2026-04-24 23:47:38 -0400

    Import Add item component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Card from './components/Cards/Card';
 import Logo from './components/Cards/Logo'
 import NavItem from './components/Buttons/NavItem';
 import CategoryCard from './components/Cards/CategoryCard';
+import AddITem from './components/Cards/AddItem';
 function App() {
 
   return (
     <>
-    <Card description={'Total items'}  amount={125} svg={'/assets/BurgerIcon.svg'}/>
-    <Logo/>
-    <NavItem text={'Caterogy'} src={'/assets/Stack.svg'}/>
-    <CategoryCard src={'/assets/Burger.svg'} color='red'/>
+    {/* <Card description={'Total items'}  amount={125} svg={'/assets/BurgerIcon.svg'}/> */}
+    {/* <Logo/> */}
+    {/* <NavItem text={'Caterogy'} src={'/assets/Stack.svg'}/> */}
+    {/* <CategoryCard src={'/assets/Burger.svg'} color='red'/> */}
+    <AddITem/>
     </>
   )
 }
