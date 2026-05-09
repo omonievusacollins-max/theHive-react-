39dc8e2c42c2a1e5f0bbf37f21e28f4d961ba076
omonievusacollins-max <omonievusacollins@gmail.com>
2026-05-09 04:46:25 -0400

    created MenuTable

---
+++ src/components/Table/MenuTable.jsx
@@ -0,0 +1,15 @@
+import menu from 
+function MenuTable(){
+    return(
+        <table className="menu-table">
+            <thead>
+                <tr>
+                    <th>Item Details</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+        </table>
+    )
+}
