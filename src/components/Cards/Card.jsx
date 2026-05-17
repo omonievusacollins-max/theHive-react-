29090a7e165bad6ca33c6689027566118b08ced3
omonievusacollins-max <omonievusacollins@gmail.com>
2026-05-17 08:36:43 -0400

    Conditional rendering for string Amount

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,14 +1,16 @@
 import styles from './card.module.css'
-function Card({description, amount, svg, color}){
+import { useState } from 'react'
+function Card({description, amount, svg, backgroundColor}){
+    
     return(
         <div className={styles.container}>
-            <div className={styles.imageContainer} style={{background: color}}>
+            <div className={styles.imageContainer} style={{backgroundColor: backgroundColor}}>
                 <img src={svg} alt="svg" />
             </div>
 
             <div className={styles.descriptionAmount}>
                 <p id={styles.description}>{description}</p>
-                <h1 id={styles.amount}>{amount}</h1>
+                <h1 id={styles.amount} style={{fontSize: typeof(amount) == 'string' ? '15px' : '20px'}}> {amount}</h1>
             </div>
         </div>
     )
