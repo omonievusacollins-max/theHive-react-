ef9eeed2fb95bfe42143072933d9c721ed66d4e9
omonievusacollins-max <omonievusacollins@gmail.com>
2026-05-17 08:39:07 -0400

    replaced color = hsla with backgroundColor = rgba instead

--- src/components/Cards/CardList.jsx
+++ src/components/Cards/CardList.jsx
@@ -3,10 +3,10 @@ import styles from './CardList.module.css';
 function CardList({ cards }) {
   return (
     <div className={styles.records}>
-        <Card description={'Total items'}  amount={125} svg={'/assets/BurgerIcon.svg'}/>
-        <Card description={'Categories'}  amount={8} svg={'/assets/BurgerIcon.svg'}/>
-        <Card description={'Available'}  amount={'Coming soon'} svg={'/assets/checkGreen.svg'} color={'hsla(128, 100, 31, 16%)'}/>
-        <Card description={'Out of Stock'}  amount={125} svg={'/assets/redBanIcon.svg'} color={'128, 100, 31, 16%'}/>
+        <Card description={'Total items'}  amount={125} svg={'/assets/BurgerIcon.svg'} backgroundColor={'rgba(255, 26, 0, 16%)'}/>
+        <Card description={'Categories'}  amount={8} svg={'/assets/categoryPurple.svg'} backgroundColor={'rgba(55, 0, 255, 16%)'} />
+        <Card description={'Available'}  amount={'Coming soon'} svg={'/assets/checkGreen.svg'} backgroundColor={'rgba(128, 100, 31, 16%)'}/>
+        <Card description={'Out of Stock'}  amount={125} svg={'/assets/redBanIcon.svg'} backgroundColor={'rgba(255, 0, 0, 16%)'}/>
     </div>
   );
 }
