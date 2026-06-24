16b2585ad2a6eed03b4f64ddf6a17268d4e528de
omonievusacollins-max <omonievusacollins@gmail.com>
2026-06-24 12:58:23 -0400

    Did some adjustment

--- src/components/Cards/Logo.jsx
+++ src/components/Cards/Logo.jsx
@@ -2,7 +2,7 @@ import './logo.css'
 function Logo(){
     return(
         <div className='logoContainer'>
-            <img src="/assets/Burgerlogo.svg" alt="" className='Burgerlogo'/>
+            {/* <img src="/assets/Burgerlogo.svg" alt="" className='Burgerlogo'/> */}
             <img src="/assets/Hive-logo.svg" alt="logo" className='logo'/>
         </div>
     )
