5cb6393a0644b300ae6e9cc259d5f9b63d178881
omonievusacollins-max <omonievusacollins@gmail.com>
2026-07-01 13:40:32 -0400

    Changes -Removes helper function capitalizeWords to it's helper folder - Define various useSates - Defined searchFilterMenu - Created addItem Function - Made sure Controls are working i.e filter by search, filter by clicked category - Display drawerTicket, sheet, overlay on various clicks //////styling////////// Few adjustments in styling

--- src/Staff.jsx
+++ src/Staff.jsx
@@ -1,20 +1,43 @@
 import './Staff.css';
 import menu from './components/Table/menu.json'
 import Search from './components/Inputs/Search';
-// import { useState, useState } from 'react';
+import { useState } from 'react';
+import { capitalizeWords } from './Utils/capitalizeFirstWords';
+
 function Staff(){
     const category = Object.keys(menu)
     const Menu = Object.entries(menu)
 
-    // function to capitalize first letter of each word in a sentence
-    function capitalizeWords(sentence){
-        if(!sentence) return;
-        return sentence.split(' ').map(word => {
-            if(!word) return;
-            return word.charAt(0).toUpperCase() + word.slice(1)
-        })
-        .join(' ');
+    // UseStates
+    const [selectedCategory, setSelectedCategory] = useState('All');
+    const [activeState, setActiveState] = useState('All');
+    const [searchValue, setSearchValue] = useState('');
+    const [totalItems, setTotalItems] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [ticketDisplay, setTicketDisplay] = useState(false)
+    const [displayDrawer, setDisplayDrawer] = useState(false)
+
+
+    const handleClickedCategory = (category, index) => {
+        setSelectedCategory(category);
+        setActiveState(index);
     }
+
+    const filteredMenu = selectedCategory === 'All' ? Menu : Menu.filter(([category, items])=> category === selectedCategory);
+
+    const searchFilterMenu = filteredMenu.map(([category, items]) => {
+        const filtereItems = items.filter(item => item.name.includes(searchValue.toLowerCase()))
+        return [category, filtereItems]
+    }).filter(([category, filtereItems]) => filtereItems.length > 0) // remove empty arrays
+    
+    const addITem = (price) => {
+        setTotalItems(prev=> prev + 1)
+        setTotalPrice(prev => prev + price)
+        setTicketDisplay(true)
+        console.log(totalItems)
+        console.log(totalPrice)
+    }
+
 
     return(
         <div className='phone'>
@@ -33,11 +56,12 @@ function Staff(){
             
             {/* CONTROLS */}
             <div className="controls">
-                <Search paddingValue={'11px 14px 11px 36px'} placeholder={'Search Menu...'}/>
+                <Search paddingValue={'11px 14px 11px 36px'} placeholder={'Search Menu...'} searchTerm={searchValue} setSearchTerm={setSearchValue}/>
                 <div className="cat-rail">
-                     <div className="cat-pill active">All</div>
+                    <div className={activeState === 'All' ? 'active cat-pill' : 'cat-pill'} onClick={()=>{setActiveState('All'); setSelectedCategory('All')}}>All</div>
+
                     {category.map((category, index) => (
-                    <div className="cat-pill" key={index}>{category}</div>
+                    <div className={activeState === index ? 'active cat-pill' : 'cat-pill'} key={index} onClick={() => {handleClickedCategory(category, index);}} >{capitalizeWords(category)}</div>
                     ))}
                 </div>
             </div>
@@ -45,34 +69,34 @@ function Staff(){
 
             <div className="cat-block">
                 {
-                    Menu.flatMap(([category, item]) => {
-                        
+                    searchFilterMenu.flatMap(([category, item]) => {
                         return [
                             <div className="cat-head" key={`${category}-`}>{capitalizeWords(category)}</div>,
                             ...item.map(meal => (
-                                <div className="food-row" data-name="Regular Beef" data-price="3800" key={`${category}-${meal.name}`}>
+                                <div className="food-row" key={`${category}-${meal.name}`} onClick={()=> addITem(meal.price)}>
                                     <div className="food-info">
                                         <div className="food-name">{capitalizeWords(meal.name)}</div>
                                         <div className="food-price">{meal.price}</div>
                                     </div>
-                                <div className="add-btn">+</div>
-                            </div>
+                                    <div className="add-btn">+</div>
+                                </div>
                             ))
                         ]
                     })
+
                 }
             </div>
 
 
             {/* collapsed drawer tab */}
-            <div className="drawer-tab" id="drawerTab">
+            <div className="drawer-tab" id="drawerTab" style={{display: ticketDisplay ? 'flex' : 'none'}} onClick={()=> setDisplayDrawer(true)}>
                 <div className="drawer-tab-left">
                     <div className="ticket-icon">
                         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.3"><path d="M6 2h9l5 5v13a2 2 0 01-2 2H6a2 2 0 01-2-2V4a2 2 0 012-2z"/></svg>
                     </div>
                     <div>
-                        <div className="drawer-count" id="drawerCount">3 items</div>
-                        <div className="drawer-total" id="drawerTotal">₦0</div>
+                        <div className="drawer-count" id="drawerCount">{totalItems} items</div>
+                        <div className="drawer-total" id="drawerTotal">₦{totalPrice}</div>
                     </div>
                 </div>
                 <div className="drawer-chevron">
@@ -80,24 +104,41 @@ function Staff(){
                 </div>
             </div>
 
-            <div className="sheet-overlay" id="overlay"></div>
+            <div className={displayDrawer ? 'sheet-overlay show' : 'sheet-overlay'} id="overlay" onClick={()=> setDisplayDrawer(false)}></div>
 
-            <div className="sheet" id="sheet">
+            <div className={displayDrawer ? 'sheet open' : 'sheet'} id="sheet">
                 <div className="sheet-handle"><span></span></div>
                 <div className="ticket-header">
                     <div className="ticket-title">Order Ticket</div>
-                    <div className="ticket-sub" id="ticketSub">0 items</div>
-                    <div className="ticket-close" id="closeSheet">✕</div>
+                    <div className="ticket-sub" id="ticketSub">{totalItems}</div>
+                    <div className="ticket-close" id="closeSheet" onClick={()=> setDisplayDrawer(false)}>✕</div>
                 </div>
 
+                {/* TICKET BODY */}
                 <div className="ticket-body" id="ticketBody">
-                    <div className="ticket-empty">No items yet</div>
+                    <div className="ticket-empty" style={{display: displayDrawer ? 'none' : 'flex'}}>No items yet</div>
+                    <div class="tline">
+                        <div class="tline-top">
+                            {/* Working on this */}
+                            <div class="tline-name">line.name</div>
+                            <div class="tline-price">₦$lineTotalline.toLocaleString</div>
+                        </div>
+                        <div class="tline-controls">
+                            <div class="tline-qty">
+                                <button>−</button>
+                                <span class="n">$line.qty</span>
+                                <button>+</button>
+                            </div>
+                            <button class="tline-remove">Remove</button>
+                        </div>
+                        <div class="addon-row">chips</div>
+                    </div>
                 </div>
 
                 <div className="ticket-footer">
                     <div className="total-line">
                         <div className="total-label">Total</div>
-                        <div className="total-amt" id="totalAmt">₦0</div>
+                        <div className="total-amt" id="totalAmt">₦{totalPrice}</div>
                     </div>
                     <div className="input-row">
                         <label className="input-label">Customer name</label>
