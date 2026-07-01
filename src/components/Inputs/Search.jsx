1a36a187e4afa79769cab2443b2068b97f98a3e7
omonievusacollins-max <omonievusacollins@gmail.com>
2026-07-01 13:40:54 -0400

    Changed searchValue back to searchTerm for simplicity

--- src/components/Inputs/Search.jsx
+++ src/components/Inputs/Search.jsx
@@ -1,9 +1,9 @@
 import './Search.css';
 import { useState } from 'react';
-function Search( {placeholder, searchValue, setSearchTerm, setDropDown, paddingValue}){
+function Search( {placeholder, searchTerm, setSearchTerm, setDropDown, paddingValue}){
     return(
         <div className='searchWrap'>
-            <input type="text" className="search" placeholder={placeholder} value={searchValue} onChange={(e) => setSearchTerm(e.target.value)} style={{padding: paddingValue}}/>
+            <input type="text" className="search" placeholder={placeholder} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} style={{padding: paddingValue}}/>
             <img src="./assets/Search.svg" alt="search icon" className='searchIcon'/>
         </div>
     )
